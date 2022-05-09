525dfb421257f91eb68a9ffd8a3fd6a259f9688a
JL710 <jan.lyncker@freenet.de>
2022-05-09 16:26:01 +0200

    add active_controll parameter/attr

--- src/BetterTurtle.py
+++ src/BetterTurtle.py
@@ -5,7 +5,7 @@ import threading
 
 
 class BetterTurtle(tkinter.Tk):
-    def __init__(self, title: str="BetterTurtle", geometry: str="500x500"):
+    def __init__(self, title: str="BetterTurtle", geometry: str="500x500", active_control: bool=True):
         super().__init__()
         self.title(title)
         self.geometry(geometry)
@@ -13,14 +13,16 @@ class BetterTurtle(tkinter.Tk):
         self.columnconfigure(7, weight=1)
         self.rowconfigure(1, weight=1)
 
+        self.active_control = active_control
+
         # create widgets
         self.turtle_canvas = tkinter.Canvas(self)
-        self.larger_button = ttk.Button(self, width=2, text="+", command=lambda: self.turtle_canvas.scale("all", 0, 0, 1.1, 1.1))
-        self.smaller_button = ttk.Button(self, width=2, text="-", command=lambda: self.turtle_canvas.scale("all", 0, 0, 0.9, 0.9))
-        self.left_button = ttk.Button(self, width=2, text="←", command=lambda: self.turtle_canvas.move("all", -10, 0)) #←↑→↓
-        self.up_button = ttk.Button(self, width=2, text="↑", command=lambda: self.turtle_canvas.move("all", 0, -10))
-        self.right_button = ttk.Button(self, width=2, text="→", command=lambda: self.turtle_canvas.move("all", 10, 0))
-        self.down_button = ttk.Button(self, width=2, text="↓", command=lambda: self.turtle_canvas.move("all", 0, 10))
+        self.larger_button = ttk.Button(self, width=2, text="+", command=self.__zoom_in)
+        self.smaller_button = ttk.Button(self, width=2, text="-", command=self.__zoom_out)
+        self.left_button = ttk.Button(self, width=2, text="←", command=self.__move_left) #←↑→↓
+        self.up_button = ttk.Button(self, width=2, text="↑", command=self.__move_up)
+        self.right_button = ttk.Button(self, width=2, text="→", command=self.__move_right)
+        self.down_button = ttk.Button(self, width=2, text="↓", command=self.__move_down)
         self.spacing_label = ttk.Label(self, text="")
 
         # grid widgets
@@ -37,6 +39,30 @@ class BetterTurtle(tkinter.Tk):
 
         thread = threading.Thread(target=lambda: self.mainloop)
         thread.start()
+
+    def __zoom_in(self):
+        if self.active_control:
+            self.turtle_canvas.scale("all", 0, 0, 1.1, 1.1)
+
+    def __zoom_out(self):
+        if self.active_control:
+            self.turtle_canvas.scale("all", 0, 0, 0.9, 0.9)
+
+    def __move_left(self):
+        if self.active_control:
+            self.turtle_canvas.move("all", -10, 0)
+
+    def __move_right(self):
+        if self.active_control:
+            self.turtle_canvas.move("all", 10, 0)
+
+    def __move_up(self):
+        if self.active_control:
+            self.turtle_canvas.move("all", 0, -10)
+    
+    def __move_down(self):
+        if self.active_control:
+            self.turtle_canvas.move("all", 0, 10)
 
     def get_turtle(self):
         return self.turtle
